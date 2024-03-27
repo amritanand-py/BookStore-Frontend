2d7ef566cd824ecf53d39780ee4ebefc416977be
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2024-03-27 17:07:40 +0530

    Add: Component Added
    Signup-Login-ForgotPass

--- Bookstore/src/app/app.module.ts
+++ Bookstore/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { LoginComponent } from './Components/login/login.component';
+import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
