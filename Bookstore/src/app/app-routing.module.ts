4f1677c1a83c2c5dea47de78807850c229b323db
Amrit Anand <64958314+amritanand-py@users.noreply.github.com>
2024-04-03 12:39:46 +0530

    Add: Routing to page is setup

--- Bookstore/src/app/app-routing.module.ts
+++ Bookstore/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashBoardComponent } from './Components/dash-board/dash-board.component';
+import { BookDetailPageComponent } from './Components/book-detail-page/book-detail-page.component';
 
 const routes: Routes = [
-  {path:'dashboard',component:DashBoardComponent}
+  {
+    path:'dashboard',
+    component:DashBoardComponent
+  },
+  {
+    path: 'Bookdetails/:id',
+    component: BookDetailPageComponent
+  }
 ];
 
 @NgModule({
